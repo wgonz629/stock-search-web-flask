6b346f0b2e7c99a8bbac469a79fe389ac2616702
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-09-15 00:57:08 -0700

    Initial structure and endpoint created (#1)
    * defined gitignore and requirements.txt
    
    * created static pages
    
    - defined initial dummy folders and files for project structure
    
    * created flask project as a package
    
    * drafted readme
    
    * update requirements.txt - dateutil and requests
    
    * blueprint defined in settings page
    
    * [WIP] defined endpoint for parsing the data
    
    - tiingo
    - newsapi

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, send_from_directory
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import parse_data
+    app.register_blueprint(parse_data.bp)
+
+    @app.route('/index.html')
+    def landing_page():
+        return send_from_directory('static','index.html')
+
+    return app
